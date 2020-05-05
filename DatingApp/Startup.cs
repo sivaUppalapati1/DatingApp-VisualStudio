f5dca6a992020cdeaee75148616acbdbaaab5b69
siva <sivavarma1496@gmail.com>
2020-05-05 22:05:49 +0530

    Added SeedData

--- DatingApp/Startup.cs
+++ DatingApp/Startup.cs
@@ -48,6 +48,7 @@ namespace DatingApp
                 };
             });
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository, DatingRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
